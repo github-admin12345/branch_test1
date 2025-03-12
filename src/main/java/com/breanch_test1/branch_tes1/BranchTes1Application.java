f3f7bf9c78ff63428cc9fc8db4f4d66a729b78fb
ram <annatram050604@gmail.com>
2025-03-12 12:51:36 +0530

    First Commit

---
+++ src/main/java/com/breanch_test1/branch_tes1/BranchTes1Application.java
@@ -0,0 +1,13 @@
+package com.breanch_test1.branch_tes1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchTes1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchTes1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/breanch_test1/branch_tes1/BranchTes1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breanch_test1.branch_tes1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchTes1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
